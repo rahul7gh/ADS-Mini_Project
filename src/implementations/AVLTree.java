9c9596d137ea6d692a986310776a361ea0899c01
Rahul Pohare <rahulpohare41@gmail.com>
2022-06-06 07:35:58 +0530

    Refactored code of previous version

--- src/implementations/AVLTree.java
+++ src/implementations/AVLTree.java
@@ -26,10 +26,6 @@ public class AVLTree<T extends Comparable<? super T>> implements AVLTreeIface<T>
 		return root;
 	}
 
-	
-//	public void setRoot(BinaryTreeNode<T> root) {
-//		this.root = root;
-//	}
 
 	public int getSize() {
 		return size;
@@ -159,9 +155,7 @@ public class AVLTree<T extends Comparable<? super T>> implements AVLTreeIface<T>
 
 	@Override
 	public BinaryTreeNode<T> delete(BinaryTreeNode<T> keyNode) {
-		
-
-		
+				
 		if(keyNode==null || root==null) return null;
 		
 		Stack<BinaryTreeNode<T>> stack= new Stack<>();
@@ -184,14 +178,13 @@ public class AVLTree<T extends Comparable<? super T>> implements AVLTreeIface<T>
 		{
 			if(current==root)
 			{
-//				retVal=current;
+
 				root=null;
-//				System.out.println(getSize());
 				return retVal;
 			}
 			else
 			{
-//				retVal=current;
+
 				if(current.compareTo(stack.peek())<=0)
 					stack.peek().setLeft(null);
 				else
@@ -280,7 +273,7 @@ public class AVLTree<T extends Comparable<? super T>> implements AVLTreeIface<T>
 		}
 		else
 		{
-//			stack.clear();
+
 			Stack<BinaryTreeNode<T>> succStack=new Stack<>();
 			BinaryTreeNode<T> successor=current.getRight();
 			BinaryTreeNode<T> successorParent=null;
@@ -398,336 +391,15 @@ public class AVLTree<T extends Comparable<? super T>> implements AVLTreeIface<T>
 			}
 				
 		}
-	
-		
-		
-//		System.out.println(getSize());
-		
-		
+
 		return retVal;
-//		return deleteRecursive(root, keyNode);
-		
-	
-		
-		
-		
-//		return deleteRecursive(root, keyNode);
 		
 	}
-	
-	private BinaryTreeNode<T> checkImbalanceAndBalanceSuccTree(Stack<BinaryTreeNode<T>> stack, BinaryTreeNode<T> deleted, BinaryTreeNode<T> current) 
-	{	
-		while(!stack.isEmpty())
-		{
-			int balanceFactor=getBalanceFactor(stack.peek());
-			if(balanceFactor>1 || balanceFactor<-1)
-				break;
-			stack.pop();
-		}
-		if(stack.isEmpty()) return deleted;
-		
-		BinaryTreeNode<T> g= stack.pop();
-		BinaryTreeNode<T> gparent=current,p=null,x=null;
-		StringBuilder imbalanceType=new StringBuilder("");
-		
-		if(!stack.isEmpty())
-			gparent=stack.pop();
-		
-		
-		if((deleted.compareTo(g))<=0)
-		{
-			imbalanceType.append("R");
-			p=g.getRight();
-			int bf=getBalanceFactor(p);
-			if(bf<=0)
-			{
-				imbalanceType.append("R");
-			}
-			else
-			{
-				imbalanceType.append("L");
-				x=p.getLeft();
-			}
-		}
-		else
-		{
-			imbalanceType.append("L");
-			p=g.getLeft();
-			int bf=getBalanceFactor(p);
-			if(bf>=0)
-			{
-				imbalanceType.append("L");
-			}
-			else
-			{
-				imbalanceType.append("R");
-				x=p.getRight();
-			}
-		}
-		
-		switch(imbalanceType.toString())
-		{
-			case "LL":
-				rightRotate(gparent, g, p);
-				break;
-				
-			case "LR":
-				leftRotate(g, p, x);
-				rightRotate(gparent, g, x);
-				break;
-				
-			case "RL":
-				rightRotate(g, p, x);
-				leftRotate(gparent, g, x);
-				break;
-				
-			case "RR":
-				leftRotate(gparent, g, p);
-				break;
-		}
-		
-		return current;
-	}
 
-
-private BinaryTreeNode<T> checkImbalanceAndBalanceTree(Stack<BinaryTreeNode<T>> stack, BinaryTreeNode<T> current,boolean isSucessor) 
-	{	boolean flag=false;
-		while(!stack.isEmpty())
-		{
-			int balanceFactor=getBalanceFactor(stack.peek());
-			if(balanceFactor>1 || balanceFactor<-1)
-				break;
-			stack.pop();
-			flag=true;
-		}
-		if(stack.isEmpty()) return current;
-	
-		StringBuilder imbalanceType=new StringBuilder("");
-		BinaryTreeNode<T> g= stack.pop();
-		BinaryTreeNode<T> gparent=null,p=null,x=null;
-		
-		if(isSucessor)
-		{
-			if(!flag)
-			{
-				imbalanceType.append("L");
-				p=g.getLeft();
-				int bf=getBalanceFactor(p);
-				if(bf>=0)
-				{
-					imbalanceType.append("L");
-				}
-				else
-				{
-					imbalanceType.append("R");
-					x=p.getRight();
-				}
-			}
-			else
-			{
-
-				if((current.compareTo(g))<=0)
-				{
-					imbalanceType.append("R");
-					p=g.getRight();
-					int bf=getBalanceFactor(p);
-					if(bf<=0)
-					{
-						imbalanceType.append("R");
-					}
-					else
-					{
-						imbalanceType.append("L");
-						x=p.getLeft();
-					}
-				}
-				else
-				{
-					imbalanceType.append("L");
-					p=g.getLeft();
-					int bf=getBalanceFactor(p);
-					if(bf>=0)
-					{
-						imbalanceType.append("L");
-					}
-					else
-					{
-						imbalanceType.append("R");
-						x=p.getRight();
-					}
-				}
-			
-			}
-			
-		}
-		else
-		{
-			if((current.compareTo(g))<=0)
-			{
-				imbalanceType.append("R");
-				p=g.getRight();
-				int bf=getBalanceFactor(p);
-				if(bf<=0)
-				{
-					imbalanceType.append("R");
-				}
-				else
-				{
-					imbalanceType.append("L");
-					x=p.getLeft();
-				}
-			}
-			else
-			{
-				imbalanceType.append("L");
-				p=g.getLeft();
-				int bf=getBalanceFactor(p);
-				if(bf>=0)
-				{
-					imbalanceType.append("L");
-				}
-				else
-				{
-					imbalanceType.append("R");
-					x=p.getRight();
-				}
-			}
-		}	
-		
-		if(!stack.isEmpty())
-			gparent=stack.pop();
-		
-		switch(imbalanceType.toString())
-		{
-			case "LL":
-				rightRotate(gparent, g, p);
-				break;
-				
-			case "LR":
-				leftRotate(g, p, x);
-				rightRotate(gparent, g, x);
-				break;
-				
-			case "RL":
-				rightRotate(g, p, x);
-				leftRotate(gparent, g, x);
-				break;
-				
-			case "RR":
-				leftRotate(gparent, g, p);
-				break;
-		}
-		
-		return current;
-	}
-
-private void swapDataOfNodes(BinaryTreeNode<T> current, BinaryTreeNode<T> successor) {
-	T temp=current.getData();
-	current.setData(successor.getData());
-	successor.setData(temp);
-}
-
-//	private BinaryTreeNode<T> deleteRecursive(BinaryTreeNode<T> node,BinaryTreeNode<T> keyNode) { 
-//		
-//		if(node==null)
-//			return node;
-//		
-//		//key<node.data
-//		if(keyNode.compareTo(node)<0) {
-//			node.setLeft(deleteRecursive(node.getLeft(),keyNode));
-//		}
-//		//key>node.data
-//		else if(keyNode.compareTo(node)>0) {
-//			node.setRight(deleteRecursive(node.getRight(),keyNode));
-//		}
-//		//key==node.data
-//		else {
-//			if ((node.getLeft() == null) || (node.getRight() == null))
-//            {
-//				BinaryTreeNode<T> temp = null;
-//                if (temp == node.getLeft())
-//                    temp = node.getRight();
-//                else
-//                    temp = node.getRight();
-// 
-//                
-//                if (temp == null)
-//                {
-//                    temp = node;
-//                    node = null;
-//                }
-//                else 
-//                    node = temp; 
-//            }else
-//            {           
-//            	BinaryTreeNode<T> temp = minValueNode(node.getRight());
-// 
-//               node.setData(temp.getData());
-//               node.setRight(deleteRecursive(node.getRight(), temp));
-//            }
-//		}
-//		
-//		if (node == null)
-//            return node;
-// 
-//       
-//        int balance = getBalanceFactor(node);
-// 
-//        
-//        // Left Left Case
-//        if (balance > 1 && getBalanceFactor(node.getLeft()) >= 0)
-//            return right(node);
-// 
-//        // Left Right Case
-//        if (balance > 1 && getBalanceFactor(node.getLeft()) < 0)
-//        {
-//        	node.setLeft(left(node.getLeft()));
-//            return right(node);
-//        }
-// 
-//        // Right Right Case
-//        if (balance < -1 && getBalanceFactor(node.getRight()) <= 0)
-//            return left(node);
-// 
-//        // Right Left Case
-//        if (balance < -1 && getBalanceFactor(node.getRight()) > 0)
-//        {
-//        	node.setRight(right(node.getRight()));
-//            return left(node);
-//        }
-// 
-//        return node;
-//	}
-	
-	private BinaryTreeNode<T> left(BinaryTreeNode<T> x) {
-		BinaryTreeNode<T> y = x.getRight();
-		BinaryTreeNode<T> T2 = y.getLeft();
- 
-        
-		y.setLeft(x);
-        x.setRight(T2);
-        
-		return y;
-	}
-
-	private BinaryTreeNode<T> right(BinaryTreeNode<T> y) {
-		BinaryTreeNode<T> x = y.getLeft();
-		BinaryTreeNode<T> T2 = x.getRight();
-	 
-			x.setRight(y);
-	        y.setLeft(T2);
-	        
-	 
-		return x;
-	}
-
-	private BinaryTreeNode<T> minValueNode(BinaryTreeNode<T> node) {
-		BinaryTreeNode<T> current = node;
-		 
-        while (current.getLeft() != null)
-        	current = current.getLeft();
- 
-        return current;
+	private void swapDataOfNodes(BinaryTreeNode<T> current, BinaryTreeNode<T> successor) {
+		T temp=current.getData();
+		current.setData(successor.getData());
+		successor.setData(temp);
 	}
 
 	@Override
@@ -737,10 +409,13 @@ private void swapDataOfNodes(BinaryTreeNode<T> current, BinaryTreeNode<T> succes
 	}
 	
 	private BinaryTreeNode<T> searchRecursive(BinaryTreeNode<T> node, BinaryTreeNode<T> key){
-		if (key.equals(node))
-	        return node;
+		
 		if(node==null)
 			return null;
+		
+		if (key.equals(node))
+	        return node;
+		
 		//key>node
 	    if (key.compareTo(node)>0)
 	       return searchRecursive(node.getRight(), key);
@@ -763,7 +438,7 @@ private void swapDataOfNodes(BinaryTreeNode<T> current, BinaryTreeNode<T> succes
 	
 	public int getBalanceFactor(BinaryTreeNode<T> node)
 	{
-		if(node==null) return 0;
+	
 		return (getHeight(node.getLeft())-1) - (getHeight(node.getRight())-1);
 	}
 
--- src/testers/Tester.java
+++ src/testers/Tester.java
@@ -10,125 +10,25 @@ import models.BinaryTreeNode;
 public class Tester {
 
 	public static void main(String[] args) {
-		
-		AVLTree<Integer> avl= new AVLTree<>();
-//		avl.add(new BinaryTreeNode<Integer>(79));
-//		avl.add(new BinaryTreeNode<Integer>(59));
-//		System.out.println(avl.levelOrderTraversal());
-//		
-////		System.out.println(avl.search(new BinaryTreeNode<Integer>(10)));
-//		avl.delete(new BinaryTreeNode<Integer>(79));
-//		System.out.println(avl.levelOrderTraversal());
-////		System.out.println(avl.search(new BinaryTreeNode<Integer>(10)));
-////		System.out.println(avl.search(new BinaryTreeNode<Integer>(20)));
-////		
-//		System.exit(0);
 		HashTable<Integer> intHash=new HashTable<>(new ModNHashFunction<Integer>());
 		Random r = new Random();
 		int size=10;
 		int[] arr= new int[size];
-//		arr=new int[] {86,241,194,785,597,848,424,704,689,1004};
-		arr=new int[] {604,927,100,590,922,382,428,290,494,243};
-		
-		
-		//,29,30,98,49,59};
-//		,102,114,123,131,140,159,166,172,185,197,200,217,226,238,241,258,261,275,281,294,304,317,324,337,348,358,368,371,383,393};
+
+//		arr=new int[] {604,927,100,590,922,382,428,290,494,243};
 		for (int i = 0; i < arr.length; i++) {
 			arr[i]=i*10+r.nextInt(10*100);
 			System.out.print(arr[i]+",");
-//			intHash.add(arr[i]);	
-			avl.add(new BinaryTreeNode<Integer>(arr[i]));
+			intHash.add(arr[i]);	
+
 		}
 		System.out.println();
-		avl.levelOrderTraversal();
-		
 		for (int i = 0; i < arr.length; i++) {
-			System.out.println(avl.search(new BinaryTreeNode<Integer>(arr[i])));
-			avl.delete(new BinaryTreeNode<Integer>(arr[i]));
-			System.out.println(avl.search(new BinaryTreeNode<Integer>(arr[i])));
 		
-			//			System.out.println(avl.getSize()+"  "+arr[i]);
-//			System.out.println(intHash.search(arr[i]));
-//			intHash.delete(arr[i]);
-//			System.out.println(intHash.search(arr[i]));
-//			System.out.println(intHash.getSize());
-//			System.out.println(avl.levelOrderTraversal());
+			System.out.println(intHash.search(arr[i]));
+			intHash.delete(arr[i]);
+			System.out.println(intHash.search(arr[i]));
 		}
-		avl.heightAndBalanceFactorOfEveryNode(avl.getRoot());
 		
-//		for (int i = 0; i < 10; i++) {
-//		
-//			avl.add(new BinaryTreeNode<Integer>(i*10+r.nextInt(10*100)));
-//		}
-//		avl.heightAndBalanceFactorOfEveryNode(avl.getRoot());
-//		avl.levelOrderTraversal();
-		
-//		avl.heightAndBalanceFactorOfEveryNode(avl.getRoot());
-//		System.out.println(avl.levelOrderTraversal());
-//		intHash.debug();
-//		for (int i = 0; i < arr.length; i++) {
-//			System.out.println("<-->");
-//			System.out.println(intHash.search(arr[i]));
-//			intHash.delete(arr[i]);
-//			System.out.println(intHash.search(arr[i]));
-//			System.out.println("<-->");
-//		}
-//		intHash.debug();
-//		intHash.add(40);
-		
-//		HashTable<String> strHash=new HashTable<>(new ModNHashFunction<String>());
-//		
-//		HashTable<Double> dobHash=new HashTable<>(new ModNHashFunction<Double>());
-//		intHash.debug();
-//		System.out.println(intHash.toString());
-//		System.out.println(intHash.search(29));
-//		intHash.delete(29);
-//		System.out.println(intHash.search(29));
-		
-//		System.out.println(intHash.search(79));
-//		intHash.delete(79);
-//		System.out.println(intHash.search(79));
-//		System.out.println("hshshsh");
-//		intHash.debug();
-//		System.out.println(intHash.search(79));
-//		intHash.delete(79);
-//		System.out.println(intHash.search(79));
-
-//		System.out.println(intHash.search(6200));
-//		intHash.delete(6200);
-//		System.out.println(intHash.search(6200));
-//		strHash.toString();
-		
-		
-//		AVLTree<Integer> avt= new AVLTree<>();
-//		Random r= new Random();
-//		int size=50;
-//		int [] arr= new int[size];
-////		arr=new int[] {130,187,195,168,19,197,97,70,244,139,14,182,49,188,74,220,213,147,163,139,26,241,115,220,23,56,222,137,220,181,108,175,85,83,118,0,188,238,74,126,170,53,150,70,92,229,25,210,88,179};
-//		for(int i=0;i<size;i++)
-//			arr[i]=r.nextInt(5*size);
-////			arr[i]=i;
-//		for(int i=0;i<size;i++)
-//		{
-//			System.out.print(arr[i]+",");
-//			avt.add(new BinaryTreeNode<Integer>(arr[i]));
-//		}
-//		System.out.println();
-//		Map<Integer, List<BinaryTreeNode<Integer>>> map = avt.levelOrderTraversal();
-//		
-//		map.entrySet().stream().forEach(
-//				entry-> entry.getValue().stream().filter(node-> node.getData()!=null)
-//				.forEach(node-> System.out.println(entry.getKey()+"<>"+node+"<>"+avt.getBalanceFactor(node)))
-//				);
-		
-		
-//		map.values().stream().forEach(
-//				
-//				list-> list.stream().filter(v -> v.getData()!=null)
-//											.forEach(node -> 
-//											System.out
-//											.println(node+"<>"+(avt.getBalanceFactor(node))) )
-//				
-//				);
 	}
 }
